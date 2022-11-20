a334148449be059a3d3362ca62d83c86bba75d67
minwei_mw <minwei_mw@163.com>
2022-11-20 20:03:52 +0800

    初始化组件search

--- react_scaffold/src/components/Search/index.jsx
+++ react_scaffold/src/components/Search/index.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class Search extends Component {
+  handleSearch = () => {
+    const {inputRef:{value:keyword}} = this
+    console.log(keyword)
+    
+  }
+
   render() {
     return (
       <section className="jumbotron">
-        <h3 className="jumbotron-heading"></h3>
+        <h3 className="jumbotron-heading">搜索GitHup用户</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />
-          &nbsp;<button>Search</button>
+          <input ref={v => this.inputRef = v} type="text" placeholder="输入关键字点击搜索" />
+          &nbsp;<button onClick={this.handleSearch}>搜索</button>
         </div>
       </section>
     );
